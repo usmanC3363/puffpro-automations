ab15deedfdfae6c831173c9e106a0f3a1aee7e9f
usmanBoi <usmankith@gmail.com>
2026-01-11 22:51:41 +0500

    +logo-svg comp, footer light-mode color chnges

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,13 +5,13 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none  disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
         primary: "body-xl dark:text-primary",
         secondary:
-          " border border-black/20 dark:bg-secondary text-secondary-foreground shadow-md dark:shadow-sm hover:bg-secondary/80",
+          " border border-black/20 dark:bg-secondary text-secondary-foreground shadow-sm dark:shadow-mycolors-white hover:bg-secondary/80",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
