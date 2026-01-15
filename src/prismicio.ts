c24c30fdbd4a1c5eb1baf7565ad98b2b1dd9abf0
usmanBoi <usmankith@gmail.com>
2026-01-15 19:27:27 +0500

    prismicio fetchOptions update

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -6,33 +6,23 @@ import {
 import { enableAutoPreviews } from "@prismicio/next";
 import sm from "../slicemachine.config.json";
 
-/**
- * The project's Prismic repository name.
- */
 export const repositoryName =
   process.env.NEXT_PUBLIC_PRISMIC_ENVIRONMENT || sm.repositoryName;
 
-/**
- * The project's Prismic route resolvers. This list determines a Prismic document's URL.
- */
 const routes: Route[] = [
   { type: "page", uid: "home", path: "/" },
   { type: "page", path: "/:uid" },
 ];
 
-/**
- * Creates a Prismic client for the project's repository. The client is used to
- * query content from the Prismic API.
- *
- * @param config - Configuration for the Prismic client.
- */
 export function createClient(config: ClientConfig = {}) {
+  const fetchOptions =
+    process.env.NODE_ENV === "production"
+      ? { cache: "force-cache", next: { tags: ["prismic"] } }
+      : { next: { revalidate: 5 } };
+
   const client = baseCreateClient(sm.apiEndpoint || repositoryName, {
     routes,
-    fetchOptions:
-      process.env.NODE_ENV === "production"
-        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
-        : { next: { revalidate: 5 } },
+    fetchOptions: fetchOptions as any,
     ...config,
   });
 
